75e08c6e2a8837addcd69b2e931edb2e27e87c71
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2023-05-27 23:02:21 +0300

    Add indicator markup

--- src/components/image-slider.jsx
+++ src/components/image-slider.jsx
@@ -1,13 +1,23 @@
 export default function ImageSlider({ images }) {
+  const indicatorWidthPercent = images.length > 0 ? 100 / images.length : 100;
+
   return (
-    <div className="w-full flex flex-row overflow-x-scroll snap-x snap-mandatory">
-      {images.map((url) => {
-        return (
-          <div key={url} className="w-full flex-shrink-0 snap-start">
-            <img src={url} />
-          </div>
-        );
-      })}
+    <div className="w-full">
+      <div className="w-full flex flex-row overflow-x-scroll snap-x snap-mandatory">
+        {images.map((url) => {
+          return (
+            <div key={url} className="w-full flex-shrink-0 snap-start">
+              <img src={url} />
+            </div>
+          );
+        })}
+      </div>
+      <div className="w-full h-0.5 relative bg-gray-300">
+        <div
+          className="h-0.5 absolute top-0 left-0 bg-gray-500"
+          style={{ width: `${indicatorWidthPercent}%` }}
+        />
+      </div>
     </div>
   );
 }
