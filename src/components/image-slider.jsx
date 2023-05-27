4fef4f7683bcbcb1d2c25877f1aec32fa9b12484
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2023-05-27 22:45:54 +0300

    Add snap

--- src/components/image-slider.jsx
+++ src/components/image-slider.jsx
@@ -1,9 +1,9 @@
 export default function ImageSlider({ images }) {
   return (
-    <div className="w-full flex flex-row overflow-x-scroll">
+    <div className="w-full flex flex-row overflow-x-scroll snap-x snap-mandatory">
       {images.map((url) => {
         return (
-          <div key={url} className="w-full flex-shrink-0">
+          <div key={url} className="w-full flex-shrink-0 snap-start">
             <img src={url} />
           </div>
         );
